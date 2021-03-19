4ca31cde2645062b678839209a78bd75ae3b2729
Lancaster Graham <Lancastergraham87@gmail.com>
2021-03-18 20:43:54 -0500

    I did it, not pretty

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // TODO: add code here
+function init() {
+    const json = fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const div = document.getElementById("container");
+
+            const buildAstronaut = (astronaut, i) => {
+            return `
+            <div class ="astronaut">
+                <div class ="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${json[i].picture}">
+            </div>
+            `
+            }
+            // json.forEach(astronaut => buildAstronaut(astronaut))
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += buildAstronaut(json, i);
+            }
+            
+        });
+    }); 
+    
+
+}
+
+
+window.addEventListener("load", init);
